7a252ef33a6638a911fd26ebad52fcaaa1fd5c86
NPA93 <npromail.01@gmail.com>
2024-05-20 00:28:45 +0200

    Main functionality upload

---
+++ script.js
@@ -0,0 +1,57 @@
+// Metric/Imperial Converter
+// La funcionalidad de la web será la siguiente:
+// - El usuario podrá introducir un número en un input y al pulsar el botón de convertir, la aplicación convertirá el número introducido a el equivalente métrico e imperial.
+
+// - Obtener los elementos del DOM: Input, botón y div donde se mostrará el resultado.
+// - Añadir un evento al botón de convertir que al hacer click llame a una función que se encargará de hacer la conversión.
+// - Crear una función que se encargue de hacer la conversión de métrico a imperial y viceversa.
+// - Mostrar el resultado en el div correspondiente utlizando innerHTML y template strings.
+
+const inputEl = document.getElementById("input-value");
+const buttonEl = document.getElementById("convert-button");
+let lentgthEl = document.getElementById("length-result");
+let volumeEl = document.getElementById("volume-result");
+let massEl = document.getElementById("mass-result");
+
+// convertir el valor introducido en el input de metros a pies, y de pies a metros  (1 pie = 0.3048 metros)  (1 metro = 3.28084 pies)
+
+function meterToFeet(meter) {
+  return meter * 3.281;
+}
+function feetToMeter(feet) {
+  return feet * 0.305;
+}
+
+function literToGallon(liter) {
+  return liter * 0.264;
+}
+function gallonToLiter(gallon) {
+  return gallon * 3.787;
+}
+
+function kilogramToPound(kilogram) {
+  return kilogram * 2.205;
+}
+function poundToKilogram(pound) {
+  return pound * 0.454;
+}
+
+buttonEl.addEventListener("click", function () {
+  let inputValue = inputEl.value;
+  let meterResult = meterToFeet(inputValue);
+  let feetResult = feetToMeter(inputValue);
+  let literResult = literToGallon(inputValue);
+  let gallonResult = gallonToLiter(inputValue);
+  let kilogramResult = kilogramToPound(inputValue);
+  let poundResult = poundToKilogram(inputValue);
+
+  lentgthEl.innerHTML = ` ${inputValue} meters = ${meterResult.toFixed(
+    3
+  )} feet | ${inputValue} feet = ${feetResult.toFixed(3)} meters`;
+  volumeEl.innerHTML = ` ${inputValue} liters = ${literResult.toFixed(
+    3
+  )} gallons | ${inputValue} gallons = ${gallonResult.toFixed(3)} liters`;
+  massEl.innerHTML = ` ${inputValue} kilograms = ${kilogramResult.toFixed(
+    3
+  )} pounds | ${inputValue} pounds = ${poundResult.toFixed(3)} kilograms`;
+});
